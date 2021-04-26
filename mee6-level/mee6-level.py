352f611d2629dd668b74c309e5604ff1bf21181b
scragly <29337040+scragly@users.noreply.github.com>
2021-04-26 19:45:03 +1000

    Implement basic level command.

---
+++ mee6-level/mee6-level.py
@@ -0,0 +1,34 @@
+from discord import Embed
+from discord.ext import commands
+from mee6_py_api import API
+
+from core import checks
+from core.models import PermissionLevel
+
+
+class Mee6Level(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.api = API(self.bot.config['guild_id'])
+        self.coll = bot.api.get_plugin_partition(self)
+
+    @checks.thread.only()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    @commands.command()
+    def level(self, ctx):
+        level = await self.api.levels.get_user_level(ctx.thread.id)
+        if level:
+            embed = Embed(
+                colour=ctx.bot.main_color,
+                description=f"MEE6 Level: {level}"
+            )
+        else:
+            embed = Embed(
+                colour=ctx.bot.error_color,
+                description="MEE6 level not found."
+            )
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Mee6Level(bot))
