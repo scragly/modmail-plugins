cb1440056ba14effa7eec4af6ef15869c1b9e45e
scragly <29337040+scragly@users.noreply.github.com>
2021-04-26 20:59:17 +1000

    Support format tags in level message.

--- mee6-level/mee6-level.py
+++ mee6-level/mee6-level.py
@@ -18,7 +18,8 @@ class Mee6Level(commands.Cog):
         """
         Sets a message to add to level responses.
 
-        Can be useful as a quick reference for role/perk level requirements.
+        Can be useful as a quick reference for role/perk level requirements,
+        and supports the same variables as `freply`.
 
         To remove any existing message, run the command without any arguments.
         """
@@ -47,19 +48,27 @@ class Mee6Level(commands.Cog):
     @commands.command()
     async def level(self, ctx):
         """
-        Shows recipient's MEE6 level.
+        Show recipient's MEE6 level.
 
-        Gets stats from the main server even if modmail threads are in a secondary server.
+        Stats are from the main server even if modmail threads are in a secondary server.
 
-        You can add a static message to include with the `set_level_msg` command.
+        You can add a message to include with the `set_level_msg` command, with support for
+        the same variables as `freply`.
         """
         level = await self.api.levels.get_user_level(ctx.thread.id, dont_use_cache=True)
         if level is not None:
             msg = await self.get_level_msg()
+            if msg:
+                msg = ctx.bot.formatter.format(
+                    msg, channel=ctx.channel, recipient=ctx.thread.recipient, author=ctx.message.author
+                )
+            else:
+                msg = Embed.Empty
+
             embed = Embed(
                 colour=ctx.bot.main_color,
                 title=f"MEE6 Level: {level}",
-                description=msg or Embed.Empty,
+                description=msg,
             )
         else:
             embed = Embed(
