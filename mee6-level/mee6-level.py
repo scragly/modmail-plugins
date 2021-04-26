92880fa1a9279df35b0fd513912f5566af2424bf
scragly <29337040+scragly@users.noreply.github.com>
2021-04-26 20:33:40 +1000

    Add support for a configurable message under the level command response.

--- mee6-level/mee6-level.py
+++ mee6-level/mee6-level.py
@@ -10,21 +10,59 @@ class Mee6Level(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.api = API(self.bot.config['guild_id'])
+        self.db = bot.api.get_plugin_partition(self)
+
+    @checks.has_permissions(PermissionLevel.ADMIN)
+    @commands.command()
+    async def set_level_msg(self, ctx, *, message):
+        """
+        Sets a message to add to level responses.
+
+        Can be useful as a quick reference for role/perk level requirements.
+        """
+        await self.db.find_one_and_update(
+            {"_id": "mee6-level"},
+            {"$set": {"level-msg": message}},
+            upsert=True,
+        )
+
+        embed = Embed(
+            colour=ctx.bot.main_color,
+            title="Level message has been set to the following:",
+            description=message or Embed.Empty
+        )
+        await ctx.send(embed=embed)
+
+    async def get_level_msg(self):
+        """Returns the configured message to include with level responses."""
+        config = await self.db.find_one({"_id": "mee6-level"})
+        if config is None:
+            return
+        return config.get("level-message")
 
     @checks.thread_only()
     @checks.has_permissions(PermissionLevel.SUPPORTER)
     @commands.command()
     async def level(self, ctx):
+        """
+        Shows recipient's MEE6 level.
+
+        Gets stats from the main server even if modmail threads are in a secondary server.
+
+        You can add a static message to include with the `set_level_msg` command.
+        """
         level = await self.api.levels.get_user_level(ctx.thread.id, dont_use_cache=True)
         if level is not None:
+            msg = await self.get_level_msg()
             embed = Embed(
                 colour=ctx.bot.main_color,
-                description=f"MEE6 Level: {level}"
+                title=f"MEE6 Level: {level}",
+                description=msg or Embed.Empty,
             )
         else:
             embed = Embed(
                 colour=ctx.bot.error_color,
-                description="MEE6 level not found."
+                title="MEE6 level not found."
             )
         await ctx.send(embed=embed)
 
