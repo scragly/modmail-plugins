916d8b28b6c967fbefe0127ff67c0fb279b0dcb0
scragly <29337040+scragly@users.noreply.github.com>
2021-09-11 05:32:11 +1000

    Add rank-roles plugin

---
+++ rank-roles/README.md
@@ -0,0 +1,19 @@
+# Rank Roles Plugin
+
+## Installation
+```
+?plugin add scragly/modmail-plugins/rank-roles@main
+```
+
+> Specifying `@main` is required as modmail expects `master` branch to be the default
+
+## Commands
+
+### `?level`
+Show recipient's level.  
+
+Stats are from the main server even if modmail threads are in a secondary server.  
+
+You can add a message to include with the `set_level_msg` command, with support for
+the same variables as `freply`.
+
---
+++ rank-roles/rank-roles.py
@@ -0,0 +1,64 @@
+import typing as t
+
+from discord import Embed
+from discord.ext import commands
+from mee6_py_api import API
+
+from core import checks
+from core.models import PermissionLevel
+
+if t.TYPE_CHECKING:
+    from discord import Message
+
+
+class RankRoles(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.api = API(self.bot.config['guild_id'])
+        self.db = bot.api.get_plugin_partition(self)
+        self._id = "rankroles"
+
+    async def get_level(self, user_id: int):
+        return await self.api.levels.get_user_level(user_id, dont_use_cache=True)
+
+    async def config(self, key: t.Optional[str] = None, **set_values):
+        if not key and not set_values:
+            return await self.db.find_one({"_id": self._id})
+
+        if key:
+            config = await self.db.find_one({"_id": self._id})
+            return config.get(key)
+
+        await self.db.find_one_and_update({"_id": self._id}, {"$set": set_values}, upsert=True)
+
+    @commands.Cog.listener()
+    async def on_thread_ready(self, thread):
+        msg: Message = thread.genesis_message
+        embed: Embed = msg.embeds[0]
+        level = await self.get_level(thread.id)
+        embed.add_field(name="Level", value=str(level) if level else "0")
+
+    @checks.thread_only()
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    @commands.command()
+    async def level(self, ctx):
+        """
+        Show recipient's level.
+
+        Stats are from the main server even if modmail threads are in a secondary server.
+
+        You can add a message to include with the `set_level_msg` command, with support for
+        the same variables as `freply`.
+        """
+        level = await self.get_level(ctx.thread.id)
+        if level is None:
+            level = 0
+        embed = Embed(
+            colour=ctx.bot.main_color,
+            title=f"Level: {level}",
+        )
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(RankRoles(bot))
